3be8035034340955844aa4c06a639b1c5adedb61
areceveur <adie.receveur@gmail.com>
2024-09-24 11:25:05 +0200

    Démarrage des tests

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,17 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {of} from "rxjs";
+import { HttpClientModule} from "@angular/common/http";
+import {SessionService} from "./services/session.service";
+import {Router} from "@angular/router";
 
 describe('AppComponent', () => {
+  let sessionServiceMock: any;
+  let fixture;
+  let app: AppComponent;
+  let routerMock: any;
+
   beforeEach(async () => {
+    sessionServiceMock = {
+      $isLogged: jest.fn(),
+      logOut: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: SessionService, useValue: sessionServiceMock },
+        { provide: Router, useValue: routerMock}
+      ]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
@@ -26,10 +47,20 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('front');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('front app is running!');
+  it('should update $isLogged observable', () => {
+    const mockIsLogged = true;
+
+    sessionServiceMock.$isLogged.mockReturnValue(of(mockIsLogged));
+
+    app.$isLogged().subscribe(isLogged => {
+      expect(isLogged).toBe(mockIsLogged);
+    })
   });
+
+  it('should log out the user', () => {
+    app.logout();
+    expect(sessionServiceMock.logOut).toHaveBeenCalled();
+    expect(routerMock.navigate).toHaveBeenCalledWith([''])
+  })
+
 });
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {SessionService} from "./services/session.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+
+  constructor(
+    private sessionService: SessionService,
+    private router: Router
+  ) {}
+
+  public $isLogged(): Observable<boolean> {
+    return this.sessionService.$isLogged();
+  }
+
+  public logout(): void {
+    this.sessionService.logOut();
+    this.router.navigate([''])
+  }
 }
